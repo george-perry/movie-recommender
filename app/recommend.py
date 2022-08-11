283b8a35e298376629012940d04b8e9453f3779a
George <gep1617@gmail.com>
2022-08-11 11:13:15 -0400

    Update reqs

--- app/recommend.py
+++ app/recommend.py
@@ -1,9 +1,7 @@
 from ast import literal_eval
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from bs4 import BeautifulSoup
 
 def recommend_from_title(df, title, filter):
     columns = ['Genres', 'Keywords', 'Cast', 'Directors']
